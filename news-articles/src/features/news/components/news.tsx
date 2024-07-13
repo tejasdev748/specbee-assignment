cf0f9f039d8e70f55bfc803e50b2f9ccbfdfc8df
tejasdev748 <53262517+tejasdev748@users.noreply.github.com>
2024-07-13 17:32:27 +0530

    News item component (#6)
    Reusable news item component

---
+++ news-articles/src/features/news/components/news.tsx
@@ -0,0 +1,34 @@
+import { Stack, Box, Typography } from "@mui/material";
+import { NewsProps } from "../../../types";
+
+import "../scss/news.scss";
+
+export default function News({
+  image,
+  publishDate,
+  headline,
+  shortArticle,
+  category,
+}: NewsProps) {
+  return (
+    <Box className="news-box" display="flex" flexDirection={"column"} gap={3.5}>
+      <Stack direction={"row"} gap={2.5}>
+        <Box component="img" src={image} />
+        <Stack>
+          <Typography variant="caption">{publishDate}</Typography>
+          <Typography component="article" variant="h6">
+            {headline}
+          </Typography>
+        </Stack>
+      </Stack>
+      <Typography
+        component="article"
+        variant="body1"
+        className="short-article-div"
+      >
+        {shortArticle}
+      </Typography>
+      <Typography variant="subtitle1">{category}</Typography>
+    </Box>
+  );
+}
