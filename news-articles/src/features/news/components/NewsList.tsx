de8b7209e0e1ea1bbe5e63ad15c1d16e3feea9ae
tejas.deogadkar <tejasdev748@gmail.com>
2024-07-15 04:09:13 +0530

    Sorted articles based on date

--- news-articles/src/features/news/components/NewsList.tsx
+++ news-articles/src/features/news/components/NewsList.tsx
@@ -46,12 +46,35 @@ export default function NewsList() {
         articleData = articleData.filter(({ author }) =>
           selectedAuthor.includes(author)
         );
+
+      if (isSortByDate) {
+        articleData = getArticlesSortedByDate(
+          articleData,
+          sortDateOption === "latest" ? true : false
+        );
+      }
       setArticles(articleData);
     } catch (err) {
       console.log(err);
     } finally {
       setShowLoader(false);
     }
+  };
+
+  const getArticlesSortedByDate = (
+    articles: ArticleDataResponse[],
+    isLatest: boolean
+  ) => {
+    const updatedArticles = [...articles];
+    updatedArticles.sort((article1, article2) => {
+      const { date: date1 } = article1;
+      const { date: date2 } = article2;
+      const result = isLatest
+        ? new Date(date2).getTime() - new Date(date1).getTime()
+        : new Date(date1).getTime() - new Date(date2).getTime();
+      return result;
+    });
+    return updatedArticles;
   };
 
   return (
